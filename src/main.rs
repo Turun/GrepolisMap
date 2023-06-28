b725e6296ec559ed00f9f40aa4b7fa82d972f1e0
Turun <github@turun.de>
2023-06-28 18:16:04 +0200

    got database built now

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,24 @@ mod model;
 mod presenter;
 mod view;
 
+use std::{sync::mpsc, thread};
+
+use message::Message;
+use view::View;
+
 use crate::presenter::Presenter;
 
 fn main() {
-    let p = Presenter::new();
-    p.start()
+    let (view_tx, self_rx) = mpsc::channel::<Message>();
+    let (self_tx, view_rx) = mpsc::channel::<Message>();
+
+    let view = View::new(view_rx, view_tx);
+
+    let mut p = Presenter::new(self_rx, self_tx);
+    let handle = thread::spawn(move || {
+        p.start();
+    });
+
+    view.start();
+    handle.join().expect("Failed to join view handle");
 }
