4266e7a5ef7fcd7356362c7bba4ca9995bc295ef
Turun <github@turun.de>
2023-06-28 18:21:26 +0200

    list dropdown entries which start with the predicate before entries that simply contain it

--- src/view/dropdownbox.rs
+++ src/view/dropdownbox.rs
@@ -52,18 +52,71 @@ impl<'a, F: FnMut(&mut Ui, &str) -> Response, V: AsRef<str>, I: Iterator<Item = 
         let mut changed = false;
         egui::popup_below_widget(ui, popup_id, &r, |ui| {
             egui::ScrollArea::vertical().show(ui, |ui| {
-                for var in it {
-                    let text = var.as_ref();
-                    // TODO better matching, potentially highlighting the matching part
-                    if !buf.is_empty() && !text.contains(&*buf) {
-                        continue;
+                // TODO highlight matching part
+
+                if buf.is_empty() {
+                    // show all options
+                    for var in it {
+                        let text = var.as_ref();
+                        if display(ui, text).clicked() {
+                            *buf = text.to_owned();
+                            changed = true;
+                            ui.memory_mut(|m| m.close_popup());
+                        }
                     }
+                } else {
+                    let it = it.collect::<Vec<V>>();
+                    let buf_match = buf.clone();
+                    if buf.to_lowercase().as_str() == buf.as_str() {
+                        // only lowercase input - filter case insensitively
+                        let matches_start =
+                            |var: &&V| var.as_ref().to_lowercase().starts_with(&*buf_match);
+                        let matches_anywhere =
+                            |var: &&V| var.as_ref().to_lowercase().contains(&*buf_match);
 
-                    if display(ui, text).clicked() {
-                        *buf = text.to_owned();
-                        changed = true;
+                        for var in it.iter().filter(matches_start) {
+                            let text = var.as_ref();
+                            if display(ui, text).clicked() {
+                                *buf = text.to_owned();
+                                changed = true;
+                                ui.memory_mut(|m| m.close_popup());
+                            }
+                        }
+                        for var in it
+                            .iter()
+                            .filter(|v| matches_anywhere(v) && !matches_start(v))
+                        {
+                            let text = var.as_ref();
+                            if display(ui, text).clicked() {
+                                *buf = text.to_owned();
+                                changed = true;
+                                ui.memory_mut(|m| m.close_popup());
+                            }
+                        }
+                    } else {
+                        // mixed case input - filter case sensitively
+                        let matches_start = |var: &&V| var.as_ref().starts_with(&*buf_match);
+                        let matches_anywhere = |var: &&V| var.as_ref().contains(&*buf_match);
 
-                        ui.memory_mut(|m| m.close_popup());
+                        for var in it.iter().filter(matches_start) {
+                            let text = var.as_ref();
+                            if display(ui, text).clicked() {
+                                *buf = text.to_owned();
+                                changed = true;
+                                ui.memory_mut(|m| m.close_popup());
+                            }
+                        }
+                        for var in it
+                            .iter()
+                            .filter(|v| matches_anywhere(v) && !matches_start(v))
+                        {
+                            let text = var.as_ref();
+                            if display(ui, text).clicked() {
+                                *buf = text.to_owned();
+                                changed = true;
+                                ui.memory_mut(|m| m.close_popup());
+                            }
+                        }
                     }
                 }
             });
