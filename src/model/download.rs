83ce098ca21cd40c0bca28cf1273abf9a36c8035
Turun <github@turun.de>
2023-06-28 18:16:04 +0200

    got database built now

--- src/model/download.rs
+++ src/model/download.rs
@@ -1,5 +1,7 @@
+use egui::FontData;
 /// this takes care of fetching the data and putting it into a database
 use std::future::Future;
+use tokio;
 
 use reqwest;
 use rusqlite;
@@ -29,42 +31,51 @@ pub struct Database {
 }
 
 impl Database {
-    pub async fn create_for_world(server_id: &str) -> Result<Self, rusqlite::Error> {
+    pub fn create_for_world(server_id: &str) -> Result<Self, rusqlite::Error> {
+        let runtime = tokio::runtime::Builder::new_current_thread()
+            .enable_all()
+            .build()
+            .unwrap();
+
+        return runtime.block_on(async { Self::async_create_for_world(server_id).await });
+    }
+
+    pub async fn async_create_for_world(server_id: &str) -> Result<Self, rusqlite::Error> {
         let reqwest_client = make_client();
 
-        let conn =
+        let mut conn =
             rusqlite::Connection::open_in_memory().expect("Failed to open in memory database");
         let data_players = download_generic(
             &reqwest_client,
-            format!("https://{}.grepolis.com/data/players.txt.gz", server_id),
+            format!("https://{}.grepolis.com/data/players.txt", server_id),
         );
         let data_alliances = download_generic(
             &reqwest_client,
-            format!("https://{}.grepolis.com/data/alliances.txt.gz", server_id),
+            format!("https://{}.grepolis.com/data/alliances.txt", server_id),
         );
         let data_towns = download_generic(
             &reqwest_client,
-            format!("https://{}.grepolis.com/data/towns.txt.gz", server_id),
+            format!("https://{}.grepolis.com/data/towns.txt", server_id),
         );
         let data_islands = download_generic(
             &reqwest_client,
-            format!("https://{}.grepolis.com/data/islands.txt.gz", server_id),
+            format!("https://{}.grepolis.com/data/islands.txt", server_id),
         );
 
-        Database::create_table_players(&conn, data_players).await?;
-        Database::create_table_alliances(&conn, data_alliances).await?;
-        Database::create_table_towns(&conn, data_towns).await?;
-        Database::create_table_islands(&conn, data_islands).await?;
-
+        Database::create_table_alliances(&mut conn, data_alliances).await?;
+        Database::create_table_players(&mut conn, data_players).await?;
+        Database::create_table_towns(&mut conn, data_towns).await?;
+        Database::create_table_islands(&mut conn, data_islands).await?;
         Ok(Self { connection: conn })
     }
 
     async fn create_table_players(
-        connection: &rusqlite::Connection,
+        connection: &mut rusqlite::Connection,
         data: impl Future<Output = Result<String, reqwest::Error>>,
     ) -> Result<(), rusqlite::Error> {
-        connection.execute(
-            "CREATE TABLE players(
+        connection
+            .execute(
+                "CREATE TABLE players(
             player_id INTEGER UNIQUE PRIMARY KEY, 
             name TEXT UNIQUE, 
             alliance_id INTEGER, 
@@ -72,26 +83,42 @@ impl Database {
             rank INTEGER, 
             towns INTEGER, 
             FOREIGN KEY(alliance_id) REFERENCES alliances(alliance_id))",
-            (),
-        )?;
-        let mut prepared_statement =
-            connection.prepare("INSERT INTO players VALUES(?1, ?2, ?3, ?4, ?5, ?6)")?;
+                (),
+            )
+            .expect("Failed to create players table");
+
+        let transaction = connection.transaction()?;
+
+        let mut prepared_statement = (&transaction)
+            .prepare("INSERT INTO players VALUES(?1, ?2, ?3, ?4, ?5, ?6)")
+            .expect("Failed to prepare statement");
         for line in data.await.expect("Failed to download player data").lines() {
             let mut values = line.split(",");
-            prepared_statement.execute((
-                values.next().expect(&format!("No player id in {}", line)),
-                values.next().expect(&format!("No player name in {}", line)),
-                values.next().expect(&format!("No alliance id in {}", line)),
-                values.next().expect(&format!("No player pts in {}", line)),
-                values.next().expect(&format!("No player rank in {}", line)),
-                values.next().expect(&format!("No player town in {}", line)),
-            ))?;
+            prepared_statement
+                .execute((
+                    values.next().expect(&format!("No player id in {}", line)),
+                    values.next().expect(&format!("No player name in {}", line)),
+                    {
+                        let text = values.next().expect(&format!("No alliance id in {}", line));
+                        if text.is_empty() {
+                            None
+                        } else {
+                            Some(text)
+                        }
+                    },
+                    values.next().expect(&format!("No player pts in {}", line)),
+                    values.next().expect(&format!("No player rank in {}", line)),
+                    values.next().expect(&format!("No player town in {}", line)),
+                ))
+                .expect(&format!("Failed to insert into players from line {}", line));
         }
+        drop(prepared_statement);
+        transaction.commit()?;
         Ok(())
     }
 
     async fn create_table_alliances(
-        connection: &rusqlite::Connection,
+        connection: &mut rusqlite::Connection,
         data: impl Future<Output = Result<String, reqwest::Error>>,
     ) -> Result<(), rusqlite::Error> {
         connection.execute(
@@ -101,11 +128,13 @@ impl Database {
             points INTEGER,
             towns INTEGER,
             members INTEGER,
-            rank INTEGER",
+            rank INTEGER)",
             (),
         )?;
+
+        let transaction = connection.transaction()?;
         let mut prepared_statement =
-            connection.prepare("INSERT INTO alliances VALUES(?1, ?2, ?3, ?4, ?5, ?6)")?;
+            transaction.prepare("INSERT INTO alliances VALUES(?1, ?2, ?3, ?4, ?5, ?6)")?;
         for line in data
             .await
             .expect("Failed to download alliance data")
@@ -121,11 +150,13 @@ impl Database {
                 values.next().expect(&format!("No ally rank in {}", line)),
             ))?;
         }
+        drop(prepared_statement);
+        transaction.commit()?;
         Ok(())
     }
 
     async fn create_table_towns(
-        connection: &rusqlite::Connection,
+        connection: &mut rusqlite::Connection,
         data: impl Future<Output = Result<String, reqwest::Error>>,
     ) -> Result<(), rusqlite::Error> {
         connection.execute(
@@ -140,13 +171,22 @@ impl Database {
             FOREIGN KEY(player_id) REFERENCES players(player_id))",
             (),
         )?;
+
+        let transaction = connection.transaction()?;
         let mut prepared_statement =
-            connection.prepare("INSERT INTO towns VALUES(?1, ?2, ?3, ?4, ?5, ?6, ?7)")?;
+            transaction.prepare("INSERT INTO towns VALUES(?1, ?2, ?3, ?4, ?5, ?6, ?7)")?;
         for line in data.await.expect("Failed to download town data").lines() {
             let mut values = line.split(",");
             prepared_statement.execute((
                 values.next().expect(&format!("No town id in {}", line)),
-                values.next().expect(&format!("No player id in {}", line)),
+                {
+                    let text = values.next().expect(&format!("No player id in {}", line));
+                    if text.is_empty() {
+                        None
+                    } else {
+                        Some(text)
+                    }
+                },
                 values.next().expect(&format!("No town name in {}", line)),
                 values.next().expect(&format!("No town x in {}", line)),
                 values.next().expect(&format!("No town y pts in {}", line)),
@@ -154,11 +194,13 @@ impl Database {
                 values.next().expect(&format!("No town points in {}", line)),
             ))?;
         }
+        drop(prepared_statement);
+        transaction.commit()?;
         Ok(())
     }
 
     async fn create_table_islands(
-        connection: &rusqlite::Connection,
+        connection: &mut rusqlite::Connection,
         data: impl Future<Output = Result<String, reqwest::Error>>,
     ) -> Result<(), rusqlite::Error> {
         connection.execute(
@@ -172,8 +214,9 @@ impl Database {
             ressource_minus TEXT)",
             (),
         )?;
+        let transaction = connection.transaction()?;
         let mut prepared_statement =
-            connection.prepare("INSERT INTO islands VALUES(?1, ?2, ?3, ?4, ?5, ?6, ?7)")?;
+            transaction.prepare("INSERT INTO islands VALUES(?1, ?2, ?3, ?4, ?5, ?6, ?7)")?;
         for line in data.await.expect("Failed to download island data").lines() {
             let mut values = line.split(",");
             prepared_statement.execute((
@@ -186,6 +229,8 @@ impl Database {
                 values.next().expect(&format!("No island less in {}", line)),
             ))?;
         }
+        drop(prepared_statement);
+        transaction.commit()?;
         Ok(())
     }
 }
--- src/presenter.rs
+++ src/presenter.rs
@@ -1,37 +1,27 @@
 use crate::message::Message;
 use crate::model::download::Database;
 use crate::model::Model;
-use crate::view::View;
 use std::sync::mpsc;
 
 pub struct Presenter {
     model: Model,
-    view: View,
     channel_tx: mpsc::Sender<Message>,
     channel_rx: mpsc::Receiver<Message>,
 }
 
 impl Presenter {
-    pub fn new() -> Self {
-        let (view_tx, self_rx) = mpsc::channel::<Message>();
-        let (self_tx, view_rx) = mpsc::channel::<Message>();
-
+    pub fn new(rx: mpsc::Receiver<Message>, tx: mpsc::Sender<Message>) -> Self {
         Self {
             model: Model::Uninitialized,
-            view: View::new(view_rx, view_tx),
-            channel_tx: self_tx,
-            channel_rx: self_rx,
+            channel_tx: tx,
+            channel_rx: rx,
         }
     }
-
-    pub fn start(self) {
-        self.view.start();
-
-        for msg in self.channel_rx {
+    pub fn start(&mut self) {
+        for msg in &self.channel_rx {
             match msg {
                 Message::SetServer(server) => {
-                    let db_future = Database::create_for_world(&server.id);
-                    let db = futures::executor::block_on(db_future).unwrap();
+                    let db = Database::create_for_world(&server.id).unwrap();
                     self.model = Model::Loaded { db }
                 }
                 Message::GotServer => todo!(),
