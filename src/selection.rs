104a7d135d3c5de9eeebcffdc6697ad25162da62
Turun <github@turun.de>
2023-08-20 18:57:34 +0200

    Move UI for Selections into their impl
    further compartmentalize the ui code.

--- src/selection.rs
+++ src/selection.rs
@@ -10,6 +10,7 @@ use std::sync::{mpsc, Arc};
 use crate::constraint::Constraint;
 use crate::message::MessageToModel;
 use crate::town::Town;
+use crate::view::Change;
 
 #[allow(clippy::module_name_repetitions)]
 #[derive(Debug, Default, Copy, Clone, PartialEq, Eq)]
@@ -110,6 +111,115 @@ impl TownSelection {
                 "Failed to send Message to Model for Selection {}",
                 &self
             ));
+    }
+
+    pub fn make_ui(
+        &mut self,
+        ui: &mut egui::Ui,
+        channel_tx: &mpsc::Sender<MessageToModel>,
+        selection_index: usize,
+    ) -> Option<Change> {
+        let mut re = None;
+
+        let _first_row_response = ui.horizontal(|ui| {
+            // TODO make the selection collapsible
+            if ui.button("+").clicked() {
+                re = Some(Change::Add);
+            }
+            if ui.button("-").clicked() {
+                re = Some(Change::Remove(selection_index));
+            }
+            if ui.button("↑").clicked() {
+                re = Some(Change::MoveUp(selection_index));
+            }
+            if ui.button("↓").clicked() {
+                re = Some(Change::MoveDown(selection_index));
+            }
+            ui.add_sized(
+                [
+                    ui.style().spacing.interact_size.x * 6.0,
+                    ui.style().spacing.interact_size.y,
+                ],
+                egui::TextEdit::singleline(&mut self.name),
+            );
+            ui.color_edit_button_srgba(&mut self.color);
+            ui.label(format!("{} Towns", self.towns.len()));
+            if self.state == SelectionState::Loading {
+                ui.spinner();
+            }
+        });
+
+        let num_constraints = self.constraints.len();
+        let mut edited_constraints = HashSet::new();
+        let mut constraint_change_action = None;
+        for (constraint_index, constraint) in self.constraints.iter_mut().enumerate() {
+            let (change, edited) = constraint.make_ui(
+                ui,
+                selection_index,
+                constraint_index,
+                constraint_index + 1 == num_constraints,
+            );
+
+            if edited {
+                edited_constraints.insert(constraint.partial_clone());
+            }
+
+            constraint_change_action = change;
+        }
+        if let Some(change) = constraint_change_action {
+            match change {
+                Change::MoveUp(index) => {
+                    if index >= 1 {
+                        self.constraints.swap(index, index - 1);
+                    }
+                }
+                Change::Remove(index) => {
+                    let _element = self.constraints.remove(index);
+                    if self.constraints.is_empty() {
+                        // ensure there is always at least one constraint
+                        self.constraints.push(Constraint::default());
+                    }
+                }
+                Change::MoveDown(index) => {
+                    if index + 1 < self.constraints.len() {
+                        self.constraints.swap(index, index + 1);
+                    }
+                }
+                Change::Add => self.constraints.push(Constraint::default()),
+            }
+        }
+
+        let refresh_complete_selection = matches!(
+            (self.state, constraint_change_action),
+            (SelectionState::NewlyCreated, _)  // reload everything if this selection is newly created (This is probably not needed, but I'll leave it in, just to be save)
+                 | (_, Some(Change::Add | Change::Remove(_))) // or if a constraint was added or removed
+        );
+        if refresh_complete_selection {
+            self.towns = Arc::new(Vec::new());
+            self.refresh(channel_tx);
+        } else if !edited_constraints.is_empty() {
+            self.state = SelectionState::Loading;
+            for constraint in &mut self
+                .constraints
+                .iter_mut()
+                .filter(|c| !edited_constraints.contains(c))
+            {
+                // the ddvs of all constraints that were not edited are invalidated.
+                constraint.drop_down_values = None;
+            }
+
+            channel_tx
+                .send(MessageToModel::FetchTowns(
+                    self.partial_clone(),
+                    edited_constraints,
+                ))
+                .expect(&format!(
+                    "Failed to send Message to Model for selection {}",
+                    &self
+                ));
+        }
+
+        re
     }
 }
 
--- src/view/sidepanel.rs
+++ src/view/sidepanel.rs
@@ -1,15 +1,8 @@
-use crate::constraint::Constraint;
-use crate::message::MessageToModel;
-use crate::selection::SelectionState;
-use crate::selection::TownSelection;
-use std::collections::HashSet;
-use std::sync::Arc;
-
 use super::Change;
 use super::View;
+use crate::selection::TownSelection;
 
 impl View {
-    #[allow(clippy::too_many_lines)] // UI Code, am I right, hahah
     pub fn ui_sidepanel(&mut self, ctx: &egui::Context) {
         egui::SidePanel::left("left panel").show(ctx, |ui| {
             ui.vertical(|ui| {
@@ -32,104 +25,8 @@ impl View {
 
                 let mut selection_change_action: Option<Change> = None;
                 for (selection_index, selection) in self.ui_data.selections.iter_mut().enumerate() {
-                    let _first_row_response = ui.horizontal(|ui| {
-                        if ui.button("+").clicked() {
-                            selection_change_action = Some(Change::Add);
-                        }
-                        if ui.button("-").clicked() {
-                            selection_change_action = Some(Change::Remove(selection_index));
-                        }
-                        if ui.button("↑").clicked() {
-                            selection_change_action = Some(Change::MoveUp(selection_index));
-                        }
-                        if ui.button("↓").clicked() {
-                            selection_change_action = Some(Change::MoveDown(selection_index));
-                        }
-                        ui.add_sized(
-                            [
-                                ui.style().spacing.interact_size.x * 6.0,
-                                ui.style().spacing.interact_size.y,
-                            ],
-                            egui::TextEdit::singleline(&mut selection.name),
-                        );
-                        ui.color_edit_button_srgba(&mut selection.color);
-                        ui.label(format!("{} Towns", selection.towns.len()));
-                        if selection.state == SelectionState::Loading {
-                            ui.spinner();
-                        }
-                    });
-
-                    let num_constraints = selection.constraints.len();
-                    let mut edited_constraints = HashSet::new();
-                    let mut constraint_change_action = None;
-                    for (constraint_index, constraint) in
-                        selection.constraints.iter_mut().enumerate()
-                    {
-                        let (change, edited) = constraint.make_ui(
-                            ui,
-                            selection_index,
-                            constraint_index,
-                            constraint_index + 1 == num_constraints,
-                        );
-
-                        if edited {
-                            edited_constraints.insert(constraint.partial_clone());
-                        }
-
-                        constraint_change_action = change;
-                    }
-                    if let Some(change) = constraint_change_action {
-                        match change {
-                            Change::MoveUp(index) => {
-                                if index >= 1 {
-                                    selection.constraints.swap(index, index - 1);
-                                }
-                            }
-                            Change::Remove(index) => {
-                                let _element = selection.constraints.remove(index);
-                                if selection.constraints.is_empty() {
-                                    // ensure there is always at least one constraint
-                                    selection.constraints.push(Constraint::default());
-                                }
-                            }
-                            Change::MoveDown(index) => {
-                                if index + 1 < selection.constraints.len() {
-                                    selection.constraints.swap(index, index + 1);
-                                }
-                            }
-                            Change::Add => selection.constraints.push(Constraint::default()),
-                        }
-                    }
-
-                    let refresh_complete_selection = matches!(
-                        (selection.state, constraint_change_action),
-                        (SelectionState::NewlyCreated, _)  // reload everything if this selection is newly created (This is probably not needed, but I'll leave it in, just to be save)
-                            | (_, Some(Change::Add | Change::Remove(_))) // or if a constraint was added or removed
-                    );
-                    if refresh_complete_selection {
-                        selection.towns = Arc::new(Vec::new());
-                        selection.refresh(&self.channel_presenter_tx);
-                    } else if !edited_constraints.is_empty() {
-                        selection.state = SelectionState::Loading;
-                        for constraint in &mut selection
-                            .constraints
-                            .iter_mut()
-                            .filter(|c| !edited_constraints.contains(c))
-                        {
-                            // the ddvs of all constraints that were not edited are invalidated.
-                            constraint.drop_down_values = None;
-                        }
-
-                        self.channel_presenter_tx
-                            .send(MessageToModel::FetchTowns(
-                                selection.partial_clone(),
-                                edited_constraints,
-                            ))
-                            .expect(&format!(
-                                "Failed to send Message to Model for Selection {}",
-                                &selection
-                            ));
-                    }
+                    selection_change_action =
+                        selection.make_ui(ui, &self.channel_presenter_tx, selection_index);
                     ui.separator();
                 }
 
