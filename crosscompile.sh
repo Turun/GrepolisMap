65e61fa4a0eba42fef5124ed2be2c029ef7fb78b
Turun <github@turun.de>
2023-08-21 13:12:32 +0200

    Automate linux gnu release build
    Musl is built by github actions, but I build locally as well. This
    workflow is now better automated.

--- crosscompile.sh
+++ crosscompile.sh
@@ -1,4 +1,13 @@
-cargo build --release \
-    --target x86_64-unknown-linux-gnu \
-    --target x86_64-pc-windows-gnu 
+# cargo build --release \
+#     --target x86_64-unknown-linux-gnu \
+#     --target x86_64-pc-windows-gnu 
 
+TURUNMAP_VERSION=$(cat Cargo.toml | rg "^version = \".*?\"$" | rg -o "\\d+\\.\\d+\\.\\d+")
+
+echo "Building for Version $TURUNMAP_VERSION"
+cargo build --release --target x86_64-unknown-linux-gnu 
+
+echo "Moving to tar ball"
+tar -czf "TurunMap-V$TURUNMAP_VERSION-x86_64-unknown-linux-gnu.tar.gz" "target/x86_64-unknown-linux-gnu/release/turunmap"
+
+echo "Done! Version $TURUNMAP_VERSION is ready for upload!"
