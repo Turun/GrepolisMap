d62823542799367d3ddf4e24d0fe0daca6a6cae2
Turun <github@turun.de>
2023-11-13 21:12:02 +0100

    Add more targets to local build
    In a futile attempt to crosscompile locally.

--- crosscompile.sh
+++ crosscompile.sh
@@ -6,8 +6,14 @@ TURUNMAP_VERSION=$(cat Cargo.toml | rg "^version = \".*?\"$" | rg -o "\\d+\\.\\d
 
 echo "Building for Version $TURUNMAP_VERSION"
 cargo build --release --target x86_64-unknown-linux-gnu 
+cargo build --release --target x86_64-pc-windows-gnu 
+cargo build --release --target x86_64-pc-windows-msvc 
+cargo build --release --target aarch64-apple-darwin
 
 echo "Moving to tar ball"
 tar -czf "TurunMap-V$TURUNMAP_VERSION-x86_64-unknown-linux-gnu.tar.gz" "target/x86_64-unknown-linux-gnu/release/turunmap"
+tar -czf "TurunMap-V$TURUNMAP_VERSION-aarch64-apple-darwin.tar.gz" "target/aarch64-apple-darwin/release/turunmap"
+zip "TurunMap-V$TURUNMAP_VERSION-x86_64-pc-windows-gnu.zip" "target/x86_64-pc-windows-gnu/release/turunmap"
+zip "TurunMap-V$TURUNMAP_VERSION-x86_64-pc-windows-msvc.zip" "target/x86_64-pc-windows-msvc/release/turunmap"
 
 echo "Done! Version $TURUNMAP_VERSION is ready for upload!"
